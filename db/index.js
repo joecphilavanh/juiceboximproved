7f7fefaf08f9b635545187f9324a4cd82c3a5e5b
Joe Philavanh <joecphilavanh@DESKTOP-PL0LMS4.localdomain>
2023-12-18 08:40:53 -0800

    edited some more

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const { Client } = require('pg') // imports the pg module
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+
 const client = new Client({
   connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
